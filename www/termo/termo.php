4376abab27266d9c52072ff412af1aed51022314
dcast78 <dcast78@gmail.com>
2016-01-04 13:01:05 +0100

    aggiornamento nomi variabili da redis
    come da http://www.raspibo.org/wiki/index.php?title=Discussione:Termostato

--- www/termo/termo.php
+++ www/termo/termo.php
@@ -36,9 +36,9 @@ $timestamp    = $redis->lRange('timestamp', $start, $end);
 $temp_esterna = $redis->lRange('temp_esterna', $start, $end);
 $termo        = $redis->lRange('termo', $start, $end);
 $rele         = $redis->lRange('rele', $start, $end);
-$temp_1       = $redis->lRange('temp_1', $start, $end);
-$temp_2       = $redis->lRange('temp_2', $start, $end);
-$temp_3       = $redis->lRange('temp_3', $start, $end);
+$temp_1       = $redis->lRange('camera', $start, $end);
+$temp_2       = $redis->lRange('cucina', $start, $end);
+$temp_3       = $redis->lRange('camerina', $start, $end);
 $min	      = $redis->lRange('min', $start, $end);
 $max	      = $redis->lRange('max', $start, $end);
 
